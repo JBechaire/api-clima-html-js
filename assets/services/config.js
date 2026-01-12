163a914d3b5382a17055eb312516691acf52606f
Joselaine Bechaire <git.bechaire@gmail.com>
2026-01-12 10:22:52 -0300

    add config.js

---
+++ assets/services/config.js
@@ -0,0 +1,9 @@
+
+const CONFIG = {
+  GEO_BASE_URL: "https://geocoding-api.open-meteo.com/v1",
+  WEATHER_BASE_URL: "https://api.open-meteo.com/v1/forecast",
+  DEFAULT_LANG: "pt",
+  DEFAULT_TIMEZONE: "auto",
+  TEMPERATURE_UNIT: "celsius",
+  WIND_SPEED_UNIT: "kmh",
+};
